302fec5157752dde333fd41e5eaed85623b8741b
Austinobravo <Austinobravo@gmail.com>
2024-05-22 12:06:45 +0100

    Added animation

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -55,39 +55,42 @@ const MobileNav = () => {
         }
       
     </div>
-    {isNavClicked && 
-      <div>
-        <div className='fixed w-screen h-screen bg-black/50 top-0 left-0 z-20' onClick={()=> {setIsNavClicking(!isNavClicked)}}>
-          <div className='text-amber-500 bg-blue-700 rounded-full p-2 hover:cursor-pointer w-fit ml-auto mt-14 mr-6'>
-              <X size={25}/>
+    <div className={`transition-all delay-200 duration-700 ease-in-out  ${isNavClicked ? 'left-0' :'translate-x-full '}`}>
+      {isNavClicked && 
+        <div className={` `}>
+          <div className={`fixed w-screen h-screen bg-black/50 top-0 left-0 z-20 `} onClick={()=> {setIsNavClicking(!isNavClicked)}}>
+            <div className='text-amber-500 bg-blue-700 rounded-full p-2 hover:cursor-pointer w-fit ml-auto mt-14 mr-6 '>
+                <X size={25}/>
+            </div>
           </div>
-        </div>
-        <div className={`bg-white w-72 z-20 h-screen fixed`}>
-          <div className='flex justify-center items-center'>
-            <Image src={`https://sme-gear.s3.amazonaws.com/1-d-passportPhoto-1710430072893-logo.webp`} alt='logo' width={150} height={100}/>
-          </div>
-          <div>
-            <ul>
-              {navLinks.map((navLink, index) => (
-                <Link key={index} href={navLink.path} className=''>
-                  <li className='border-b font-bold text-sm py-2 mx-4 hover:text-blue-700 transition-all' onClick={()=> {setIsNavClicking(!isNavClicked)}}>{navLink.name}</li>
-                </Link>
-              ))}
-            </ul>
-          </div>
-          <div className='flex justify-center items-center gap-6 bottom-20 w-full absolute'>
-                {socialLinks.map(((socials, index) => (
-                  <Link key={index} href={socials.link} className='hover:text-amber-500 text-blue-700'>
-                    {socials.image}
+          <div className={`bg-white w-72 z-20 h-screen fixed `}>
+            <div className='flex justify-center items-center'>
+              <Image src={`https://sme-gear.s3.amazonaws.com/1-d-passportPhoto-1710430072893-logo.webp`} alt='logo' width={150} height={100}/>
+            </div>
+            <div>
+              <ul>
+                {navLinks.map((navLink, index) => (
+                  <Link key={index} href={navLink.path} className=''>
+                    <li className='border-b font-bold text-sm py-2 mx-4 hover:text-blue-700 transition-all' onClick={()=> {setIsNavClicking(!isNavClicked)}}>{navLink.name}</li>
                   </Link>
-                )))}
-          </div>
-          <div className='text-center opacity-70 text-sm bottom-10 absolute w-full'>
-            <p>Copyright &copy; {new Date().getFullYear()}. All rights reserved</p>
+                ))}
+              </ul>
+            </div>
+            <div className='flex justify-center items-center gap-6 bottom-20 w-full absolute'>
+                  {socialLinks.map(((socials, index) => (
+                    <Link key={index} href={socials.link} className='hover:text-amber-500 text-blue-700'>
+                      {socials.image}
+                    </Link>
+                  )))}
+            </div>
+            <div className='text-center opacity-70 text-sm bottom-10 absolute w-full'>
+              <p>Copyright &copy; {new Date().getFullYear()}. All rights reserved</p>
+            </div>
           </div>
         </div>
-      </div>
-    }
+      }
+
+    </div>
     </>
   )
 }
